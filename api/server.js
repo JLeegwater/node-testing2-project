5fb317fdf0dc7ca5f6ec8272f46d20a753e5ff15
Jesse-Leegwater <Jleegwater@live.com>
2021-09-15 21:38:12 -0700

    created server.js

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const { restricted } = require("./middleware/restricted.js");
+
+const authRouter = require("./auth/auth-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/auth", authRouter);
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
